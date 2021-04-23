2ad227baadeb56742d9e7fb63c2e3f090a8e32a8
Marcos Vinicius <marcoswebmaster@hotmail.com>
2021-04-23 18:56:27 -0300

    update 23-04

--- client/src/hooks/use-wishlist/index.tsx
+++ client/src/hooks/use-wishlist/index.tsx
@@ -1,5 +1,9 @@
-import { createContext, useContext } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { GameCardProps } from 'components/GameCard'
+import { useSession } from 'next-auth/client'
+import { useQueryWishlist } from 'graphql/queries/wishlist'
+import { QueryWishlist_wishlists_games } from 'graphql/generated/QueryWishlist'
+import { gamesMapper } from 'utils/mappers'
 
 export type WishlistContextData = {
   items: GameCardProps[]
@@ -24,15 +28,32 @@ export type WishlistProviderProps = {
   children: React.ReactNode
 }
 const WishlistProvider = ({ children }: WishlistProviderProps) => {
-  const isInWishlist = (id: string) => false
+  const [session] = useSession()
+  const { data, loading } = useQueryWishlist({
+    skip: !session?.user?.email,
+    context: { session },
+    variables: {
+      identifier: session?.user?.email as string
+    }
+  })
+  const [wishlistItems, setWishlistItems] = useState<
+    QueryWishlist_wishlists_games[]
+  >([])
+  useEffect(() => {
+    setWishlistItems(data?.wishlists[0]?.games || [])
+  }, [data])
+  const isInWishlist = (id: string) =>
+    !!wishlistItems.find((game) => game.id === id)
   const addToWishlist = (id: string) => {}
   const removeFromWishlist = (id: string) => {}
   return (
     <WishlistContext.Provider
       value={{
+        items: gamesMapper(wishlistItems),
         isInWishlist,
         addToWishlist,
-        removeFromWishlist
+        removeFromWishlist,
+        loading
       }}
     >
       {children}
