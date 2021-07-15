cd8962fccadb3f365231c592bc5ef11d3012e6f4
marcoswebmasteroficial <48739175+marcoswebmasteroficial@users.noreply.github.com>
2021-07-14 21:02:21 -0300

    Update plugins.js

--- api/config/env/production/plugins.js
+++ api/config/env/production/plugins.js
@@ -2,9 +2,16 @@ module.exports = ({ env }) => ({
   email: {
     provider: "nodemailer",
     providerOptions: {
-      host: "localhost",
-      port: 1025,
-      ignoreTLS: true,
+      host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+      port: env("SMTP_PORT", 587),
+      auth: {
+        user: env("SMTP_USERNAME"),
+        pass: env("SMTP_PASSWORD"),
+      },
+    },
+    settings: {
+      defaultFrom: "wongames@gmail.com",
+      defaultReplyTo: "contact@gmail.com",
     },
   },
   upload: {
