dee17cb29a8ffcdb79003bed8a14b47118a31dd4
Marcos Vinicius <marcoswebmaster@hotmail.com>
2021-08-14 12:14:20 -0300

    update

--- client/src/components/GameCardList/stories.tsx
+++ client/src/components/GameCardList/stories.tsx
@@ -1,9 +1,21 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { GameCardProps } from 'components/GameCard'
 import GameCardList from '.'
-
+import items from './mock'
 export default {
-  title: 'GameCardList',
-  component: GameCardList
+  title: 'Game/GameCardList',
+  component: GameCardList,
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <GameCardList />
+export const Default: Story<GameCardProps[]> = (args) => (
+  <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+    <GameCardList items={args} {...args} />
+  </div>
+)
